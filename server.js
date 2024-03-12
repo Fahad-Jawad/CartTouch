2d30cceb1abb63583c493d55f387353fb977743e
Fahad Jawad <fahadjawad596@gmail.com>
2024-03-12 22:20:05 +0500

    added new get api

--- server.js
+++ server.js
@@ -60,6 +60,9 @@ app.post("/send-sms", (req, res) => {
     });
 });
 
+app.get('/',(req,res)=>{
+  res.json({message:"Server is Up"})
+})
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port:${port}`);

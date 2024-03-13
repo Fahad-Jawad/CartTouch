e586536ec11d155242184bce1ebd3ed6ebcb77a1
Fahad Jawad <fahadjawad596@gmail.com>
2024-03-13 21:35:25 +0500

    added env and git ignore

--- server.js
+++ server.js
@@ -3,16 +3,18 @@ const bodyParser = require("body-parser");
 const twilio = require("twilio");
 const cors = require("cors");
 const User = require("./firebase");
+const dotenv = require("dotenv");
 
 const app = express();
-const port = process.env.PORT || 3030;app.use(cors());
+const port = process.env.PORT || 3030;
+app.use(cors());
 
 app.use(bodyParser.json());
 
-const accountSid = "AC583ca9cf2f5b7aab08790ab7c0c6ddb6";
-const authToken = "dc875985ec1c4d0587f8e2b9094d4a9e";
-const twilioNumber = "+14243641210";
-const recipientNumber = "+923334202460";
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioNumber = process.env.TWILIO_NUMBER;
+const recipientNumber = process.env.RECIPIENT_NUMBER;
 
 const twilioClient = twilio(accountSid, authToken);
 
@@ -43,7 +45,7 @@ app.post("/send-sms", (req, res) => {
   res.header("Access-Control-Allow-Methods", "POST");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   const { sms, phone } = req.body;
-  console.log('bd',req.body,sms,phone)
+  console.log("bd", req.body, sms, phone);
   twilioClient.messages
     .create({
       body: sms,
@@ -60,9 +62,9 @@ app.post("/send-sms", (req, res) => {
     });
 });
 
-app.get('/',(req,res)=>{
-  res.json({message:"Server is Up"})
-})
+app.get("/", (req, res) => {
+  res.json({ message: "Server is Up" });
+});
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port:${port}`);
